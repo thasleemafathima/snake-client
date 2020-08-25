158f137f464fe12db79a6100b835a8f195fc0c98
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-25 20:06:16 +0000

    added name to the snake

--- client.js
+++ client.js
@@ -11,15 +11,19 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
   
-  
   conn.on('connect', () => {
-    conn.write('Hello from client!');
-    
+    conn.write("Name: TFP");
   });
+  
   
 
   return conn;
--- play.js
+++ play.js
@@ -1,31 +1,7 @@
+
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-    
-  });
-  
-
-  return conn;
-}
-
-console.log('Connecting ...');
-connect();
+const { connect } = require('./client');
 
 
